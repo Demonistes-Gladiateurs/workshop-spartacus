65eb282ee51bcce7b2bc6231e011618dcdbe58da
Jeremy0307 <jeremyderive@gmail.com>
2020-11-05 12:11:01 +0100

    PatrolScript

---
+++ Spartacus-Workshop/Assets/Scripts/AI/Patrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _startWaitTime = 5f;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
+    [SerializeField] Transform _moveSpots;
+
+
+
+    private float _waitTime;
+
+    void Start()
+    {
+        _waitTime = _startWaitTime;
+
+        _moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _moveSpots.position, _speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, _moveSpots.position) < 0.2f)
+        {
+            if (_waitTime <= 0){
+                _moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                _waitTime = _startWaitTime;
+            } else {
+                _waitTime -= Time.deltaTime;
+            }
+            }
+        }
+        
+    }
+
