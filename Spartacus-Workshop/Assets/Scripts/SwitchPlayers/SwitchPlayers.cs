71068632581005157e21367576f4e6b397bcd105
Jeremy0307 <jeremyderive@gmail.com>
2020-11-10 14:29:51 +0100

    SwitchPlayersNormé

--- Spartacus-Workshop/Assets/Scripts/SwitchPlayers/SwitchPlayers.cs
+++ Spartacus-Workshop/Assets/Scripts/SwitchPlayers/SwitchPlayers.cs
@@ -4,86 +4,84 @@ using UnityEngine;
 
 public class SwitchPlayers : MonoBehaviour
 {
-    public GameObject player1;
-    public GameObject player2;
+    [SerializeField] private GameObject _player1;
+    [SerializeField] private GameObject _player2;
 
-    public GameObject cam3;
-    public GameObject cam1;
+    [SerializeField] private GameObject _cam3;
+    [SerializeField] private GameObject _cam1;
 
     void Start()
     {
-        player2 = GameObject.Find("Player2");
-        player1 = GameObject.Find("Player1");
+        _player2 = GameObject.Find("Player2");
+        _player1 = GameObject.Find("Player1");
 
-        cam3 = GameObject.Find("Camera3");
-        cam1 = GameObject.Find("Camera1");
+        _cam3 = GameObject.Find("Camera3");
+        _cam1 = GameObject.Find("Camera1");
 
+        _cam3.SetActive(false);
 
+        _player2.GetComponent<CharacterController>().enabled = false;
 
-        cam3.SetActive(false);
+        _player2.GetComponent<FollowCharacter>().enabled = true;
 
-        player2.GetComponent<CharacterController>().enabled = false;
-
-        player2.GetComponent<FollowCharacter>().enabled = true;
-
-        cam1.tag = "MainCamera";
-        cam3.tag = "Camera 2";
+        _cam1.tag = "MainCamera";
+        _cam3.tag = "Camera 2";
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))    //"O" for "Other" or "Other Player" 
         {
-            if (player1.GetComponent<CapsuleCollider>().enabled == true)    //collider arbitrary, but reflects object state
+            if (_player1.GetComponent<CapsuleCollider>().enabled == true)    //collider arbitrary, but reflects object state
             {
-                player1.GetComponent<Rigidbody>().isKinematic = true;
-                player2.GetComponent<Rigidbody>().isKinematic = false;
+                _player1.GetComponent<Rigidbody>().isKinematic = true;
+                _player2.GetComponent<Rigidbody>().isKinematic = false;
 
-                player1.GetComponent<CapsuleCollider>().enabled = false;
-                player2.GetComponent<CapsuleCollider>().enabled = true;
+                _player1.GetComponent<CapsuleCollider>().enabled = false;
+                _player2.GetComponent<CapsuleCollider>().enabled = true;
 
 
-                player1.GetComponent<CharacterController>().enabled = false;
+                _player1.GetComponent<CharacterController>().enabled = false;
 
-                player2.GetComponent<CharacterController>().enabled = true;
+                _player2.GetComponent<CharacterController>().enabled = true;
 
-                player1.GetComponent<FollowCharacter>().enabled = true; 
-                player2.GetComponent<FollowCharacter>().enabled = false;
+                _player1.GetComponent<FollowCharacter>().enabled = true; 
+                _player2.GetComponent<FollowCharacter>().enabled = false;
 
-                cam1.SetActive(false);
+                _cam1.SetActive(false);
 
-                cam3.SetActive(true);
+                _cam3.SetActive(true);
 
-                cam1.tag = "Camera 2";
-                cam3.tag = "MainCamera";
+                _cam1.tag = "Camera 2";
+                _cam3.tag = "MainCamera";
             }
         }
 
         if (Input.GetKeyDown(KeyCode.P))   //"P" for "Player" 
         {
-            if (player1.GetComponent<CapsuleCollider>().enabled == false)   //collider arbitrary, but reflects object state
+            if (_player1.GetComponent<CapsuleCollider>().enabled == false)   //collider arbitrary, but reflects object state
             {
-                player1.GetComponent<Rigidbody>().isKinematic = false;
-                player2.GetComponent<Rigidbody>().isKinematic = true;
+                _player1.GetComponent<Rigidbody>().isKinematic = false;
+                _player2.GetComponent<Rigidbody>().isKinematic = true;
 
-                player1.GetComponent<CapsuleCollider>().enabled = true;
-                player2.GetComponent<CapsuleCollider>().enabled = false;
+                _player1.GetComponent<CapsuleCollider>().enabled = true;
+                _player2.GetComponent<CapsuleCollider>().enabled = false;
 
 
-                player1.GetComponent<CharacterController>().enabled = true;
+                _player1.GetComponent<CharacterController>().enabled = true;
 
-                player2.GetComponent<CharacterController>().enabled = false;
+                _player2.GetComponent<CharacterController>().enabled = false;
 
-                player1.GetComponent<FollowCharacter>().enabled = false;
-                player2.GetComponent<FollowCharacter>().enabled = true; ;
+                _player1.GetComponent<FollowCharacter>().enabled = false;
+                _player2.GetComponent<FollowCharacter>().enabled = true; ;
 
-                cam1.SetActive(true);
-                cam3.tag = "Camera 2";
+                _cam1.SetActive(true);
+                _cam3.tag = "Camera 2";
+                
+                _cam1 = GameObject.Find("Camera1");
+                _cam1.tag = "MainCamera";
 
-                cam1 = GameObject.Find("Camera1");
-                cam1.tag = "MainCamera";
-
-                cam3.SetActive(false);
+                _cam3.SetActive(false);
 
             }
         }
