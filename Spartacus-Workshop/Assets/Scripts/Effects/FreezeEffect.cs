5759205d782f36b4a11cf019d63a0b2bcd2dcb45
Jeremy0307 <jeremyderive@gmail.com>
2020-11-11 09:35:26 +0100

    FreezeEffect

---
+++ Spartacus-Workshop/Assets/Scripts/Effects/FreezeEffect.cs
@@ -0,0 +1,25 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeEffect : MonoBehaviour
+{
+    [SerializeField] private float _freezeDelay = 5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    IEnumerator Freeze(GameObject Go)
+    {
+        if(Go.gameObject.tag == "Player 1 || Player 2")
+        {
+            Go.GetComponent<CharacterController>().enabled = false;
+            Debug.Log("CharacterController desactivé");
+            yield return new WaitForSeconds(_freezeDelay);
+            Go.GetComponent<CharacterController>().enabled = true;
+            Debug.Log("CharacterController activé");
+        }
+    }
+}*/
