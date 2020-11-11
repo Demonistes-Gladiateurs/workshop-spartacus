6ac06aa9ad4527dec7c83a32c07d0451a8461e89
Keroro <37291988+RMaxence@users.noreply.github.com>
2020-11-11 12:36:24 +0100

    Fusion CharactController

--- Spartacus-Workshop/Assets/Scripts/CharacterScripts/CharacterController.cs
+++ Spartacus-Workshop/Assets/Scripts/CharacterScripts/CharacterController.cs
@@ -7,6 +7,13 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _speedBoost;
 
+    [SerializeField] private int _maxHealth = 500;
+    public int _currentHealth { get; private set; }
+
+    public Stat _damage;
+    public Stat _armor;
+    public Stat _heal;
+
     private Rigidbody _rb;
 
     //private bool _isOnTheGround = true;
@@ -25,6 +32,7 @@ public class CharacterController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _currentHealth = _maxHealth;
     }
 
     void Update()
@@ -38,6 +46,25 @@ public class CharacterController : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             _speed -= _speedBoost;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= _armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        _currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damages.");
+
+        if (_currentHealth <= 0)
+        {
+            Debug.Log("DEAD");
         }
     }
 
@@ -56,11 +83,40 @@ public class CharacterController : MonoBehaviour
        
     }
 
-   /* private void OnCollisionEnter(Collision collision) // Si je rentre en collision avec le gameobject au nom de "Ground" alors je peux resauter
+    /* private void OnCollisionEnter(Collision collision) // Si je rentre en collision avec le gameobject au nom de "Ground" alors je peux resauter
+     {
+         if (collision.gameObject.tag == "Ground")
+         {
+             _isOnTheGround = true;
+         }
+     }*/
+
+    public void SetDamageValue(int damageModifier)
     {
-        if (collision.gameObject.tag == "Ground")
+        _damage.RemoveModifier(damageModifier);
+        _damage.AddModifier(damageModifier);
+        Debug.Log(_damage.GetValue());
+    }
+
+    public void SetArmorValue(int armorModifier)
+    {
+        _armor.RemoveModifier(armorModifier);
+        _armor.AddModifier(armorModifier);
+        Debug.Log(_armor.GetValue());
+    }
+
+    public void SetLifeValue(int lifeModifier)
+    {
+        _heal.AddModifier(lifeModifier);
+        _currentHealth += _heal.GetValue();
+
+        if (_currentHealth >= _maxHealth)
         {
-            _isOnTheGround = true;
+            _currentHealth = _maxHealth;
         }
-    }*/
+
+        Debug.Log(_heal.GetValue());
+        Debug.Log(_currentHealth);
+        _heal.RemoveModifier(lifeModifier);
+    }
 }
