724b7c825f237e177a6cc747b90636c6575ed04b
Jeremy0307 <jeremyderive@gmail.com>
2020-11-15 12:08:11 +0100

    MainMenu:PlayAndQuit

---
+++ Spartacus-Workshop/Assets/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Play( string MaxenceScene)
+    {
+        SceneManager.LoadScene(MaxenceScene);
+    }
+}
