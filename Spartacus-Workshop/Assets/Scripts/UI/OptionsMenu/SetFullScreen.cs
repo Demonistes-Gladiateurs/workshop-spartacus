a5cae890b3e592a6f2a8e6ccd00c146100eb602b
Jeremy0307 <jeremyderive@gmail.com>
2020-11-16 11:07:10 +0100

    MainMenu/OptionsMenu/Credits

---
+++ Spartacus-Workshop/Assets/Scripts/UI/OptionsMenu/SetFullScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFullScreen : MonoBehaviour
+{
+   public void FullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
