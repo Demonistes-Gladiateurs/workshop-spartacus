cb4417f36bd490a2bf715ebed6e7458ebb37838a
eiramew <marie.najean2@gmail.com>
2020-11-16 17:07:20 +0100

    Proto_map_import_V1

---
+++ Spartacus-Workshop/Assets/Scripts/ChangeResolution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeResolution : MonoBehaviour
+{
+    Resolution[] _resolutions;
+
+    [SerializeField] private Dropdown _resolutionsDropdown;
+
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+
+        _resolutions = Screen.resolutions;
+        
+        _resolutionsDropdown.ClearOptions();
+
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 22;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add(option);
+
+            if(_resolutions[i].width == Screen.currentResolution.width &&
+               _resolutions[i].width == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        _resolutionsDropdown.AddOptions(options);
+        _resolutionsDropdown.value = currentResolutionIndex;
+        _resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
