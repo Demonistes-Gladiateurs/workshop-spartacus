063347687e9fb2d9c3be32c186b736b14bc156c9
Lilyweird <61269558+Lilyweird@users.noreply.github.com>
2020-11-17 10:53:02 +0100

    Animation
    Aleko et Nela

---
+++ Spartacus-Workshop/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0f;
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * _speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
