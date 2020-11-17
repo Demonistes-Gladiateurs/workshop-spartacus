8dafa80d05325174d7d0c930d4412347027925e2
Jeremy0307 <jeremyderive@gmail.com>
2020-11-17 11:06:36 +0100

    PauseMenu(ebauche)

---
+++ Spartacus-Workshop/Assets/PauseMenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _pauseMenuUI;
+
+    public bool _isPaused;
+
+    private void Start()
+    {
+        _pauseMenuUI.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(_isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isPaused = false;
+    }
+}
